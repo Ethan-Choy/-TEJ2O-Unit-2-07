2592c4f4cff151552ad983bfe0f62f4d505da7b2
Ethan-Choy <ethan.choy@stu.ocsb.ca>
2025-09-30 13:23:32 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ethan Choy
+ * Created on: Sep 2025
+ * This program is cookie clicker
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let numberOfCookies = 0
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    numberOfCookies = numberOfCookies + 1
+    basic.showNumber(numberOfCookies)
+
+})
+
+input.onButtonPressed(Button.B, function() {
+    basic.clearScreen()
+    basic.showNumber(numberOfCookies = 0)
+    
+})
